77fb034394f2a1aba4eab0c7c5c3c0685f182fdd
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-02-18 12:49:03 +0530

    fix pil open

--- data.py
+++ data.py
@@ -17,9 +17,6 @@ class Dataset:
             class1 = class2 = np.random.choice(self.df[self.level])
 
             path1, path2 = np.random.choice(self.df[self.df[self.level]==class1]['image_path'], size=2, replace=False)
-
-            image1 = Image.open(path1)
-            image2 = Image.open(path2)
         else: 
             label = 0
             class1, class2 = np.random.choice(self.df[self.level], size=2, replace=False)
@@ -27,8 +24,8 @@ class Dataset:
             path1 = np.random.choice(self.df[self.df[self.level]==class1]['image_path'])
             path2 = np.random.choice(self.df[self.df[self.level]==class2]['image_path'])
 
-            image1 = Image.open(path1)
-            image2 = Image.open(path2)
+        image1 = Image.open(path1).convert('RGB')
+        image2 = Image.open(path2).convert('RGB')
         if self.transform is not None:
             image1 = self.transform(image1)
             image2 = self.transform(image2)
