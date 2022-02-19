25748f01c61b2d9d5f7334c679b11659d03bc590
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-02-19 08:13:10 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-from torchvision import Dataset
+from torch.utils.data import Dataset
 
 class TrainDataset:
     def __init__(self, df, transform=None, level='species', samples=1000):
